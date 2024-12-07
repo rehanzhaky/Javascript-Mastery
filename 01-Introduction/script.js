eecccd6302351096180d419ea8367c001263dcc9
rehanzhaky <hannn9491@gmail.com>
2024-12-07 13:07:56 +0700

    First Commit

---
+++ 01-Introduction/script.js
@@ -0,0 +1,6 @@
+// Implementasi JavaScript External
+document.getElementById("klik").onclick = function () {
+  alert(
+    "Selamat datang dan selamat belajar JavaScript External bersama Raihan Zhaky"
+  );
+};
