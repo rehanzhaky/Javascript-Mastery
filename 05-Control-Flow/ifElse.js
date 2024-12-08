c84a6f3802bb627aeb8eb6a3be3f741b22ff0a92
rehanzhaky <hannn9491@gmail.com>
2024-12-08 09:10:21 +0700

    add Control Flow

---
+++ 05-Control-Flow/ifElse.js
@@ -0,0 +1,10 @@
+let nilaiStandard = 75
+let nilaiRaihan = 80
+
+if(nilaiRaihan > nilaiStandard) {
+    console.log("Selamat kamu Lulus Memuaskan!");
+} else if (nilaiRaihan == nilaiStandard) {
+    console.log("Selamat kamu Lulus dengan nilai Standard!");
+} else {
+    console.log("Kamu tinggal kelas!");
+}
---
+++ 05-Control-Flow/switchCase.js
@@ -0,0 +1,15 @@
+let lampuSekarang = "Merah";
+
+switch (lampuSekarang) {
+  case "Hijau":
+    console.log("Silahkan Jalan");
+    break;
+  case "Kuning":
+    console.log("Hati hati");
+    break;
+  case "Merah":
+    console.log("Berhenti!");
+    break;
+  default:
+    console.log("Lampu lalu lintas sedang rusak!");
+}
