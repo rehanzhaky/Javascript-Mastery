58fa4263de1c28c2e581189703ed88dc29c8d5ab
rehanzhaky <hannn9491@gmail.com>
2024-12-08 15:01:38 +0700

    add String Template

---
+++ 07-String-Template/stringTemplate.js
@@ -0,0 +1,5 @@
+let nama = "Raihan";
+let tanggal = "04 Desember 2028";
+let tempat = "Hotel Marriot";
+
+console.log(`Kepada bapak ${nama} dan keluarga dengan senang hati kami mengundang pada ${tanggal} di ${tempat}`);
