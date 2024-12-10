562201e0ed90ec38dcc9f754386f35b4db82a884
rehanzhaky <hannn9491@gmail.com>
2024-12-10 09:58:16 +0700

    add data type convertion

---
+++ 00-Project/01-Exercise/dataTypeConvertion.js
@@ -0,0 +1,12 @@
+// inisiasi satu variabel dengan nilai "100"
+// konversikan variabel diatas menjadi tipe data number lalu tambah dengan angka 50
+// inisiasi satu variabel dengan nilai "true"
+// konversikan variabel diatas menjadi tipe data boolean
+
+let nilai = "100";
+let nilaiKonversi = Number(nilai);
+console.log(`Hasil : ${nilaiKonversi + 50}`);
+
+let fakta = "true";
+let faktaKonversi = Boolean(fakta);
+console.log(`Hasil : ${faktaKonversi}`);
