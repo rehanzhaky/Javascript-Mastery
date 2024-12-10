b8f71459d3247d809f9e20ec6d563f459eb62f02
rehanzhaky <hannn9491@gmail.com>
2024-12-10 09:53:23 +0700

    add String Operation

---
+++ 00-Project/01-Exercise/stringOperation.js
@@ -0,0 +1,9 @@
+// inisiasi dua variabel string
+// gabung kedua variabel string tersebut menggunakan operator +
+// cetak variabel gabungan tersebut beserta jumlah karakternya menggunakan .length
+
+let namaPertama = "Raihan";
+let namaKedua = "Zhaky";
+let namaLengkap = namaPertama + namaKedua;
+
+console.log(namaLengkap.length);
