73b923c8bb8888b9316eb5e6080b5971693c9ee8
rehanzhaky <hannn9491@gmail.com>
2024-12-10 11:33:26 +0700

    add boolean comparation

---
+++ 00-Project/01-Exercise/booleanComparation.js
@@ -0,0 +1,12 @@
+// inisiasi dua variabel dengan nilai 20 dan 15
+// periksa hasil dari dua variabel diatas apakah?
+// nilai 1 lebih besar dari nilai 2
+// nilai 1 sama dengan nilai 2
+// nilai 1 tidak sama dengan nilai 2
+
+let nilai1 = 20;
+let nilai2 = 15;
+
+console.log(nilai1 > nilai2);
+console.log(nilai1 == nilai2);
+console.log(nilai1 != nilai2);
