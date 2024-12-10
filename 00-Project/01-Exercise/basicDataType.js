53d7d381bf63bd9e241e49eaca0ae4e01550e2d1
rehanzhaky <hannn9491@gmail.com>
2024-12-10 09:41:26 +0700

    add data type excercise

---
+++ 00-Project/01-Exercise/basicDataType.js
@@ -0,0 +1,15 @@
+// inisiasi 3 variabel dengan tipe data String, Number, dan Boolean
+// cetak variabel nya menggunakan typeof
+
+let nama = "Raihan";
+let tinggi = 173;
+let apakahManusia = true;
+
+console.log(typeof nama);
+console.log(typeof tinggi);
+console.log(typeof apakahManusia);
+
+
+
+
+
