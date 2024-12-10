ce0c880ea00183fa80f10927a1e3248a55a169e9
rehanzhaky <hannn9491@gmail.com>
2024-12-10 11:17:35 +0700

    add number operation

---
+++ 00-Project/01-Exercise/numberOperation.js
@@ -0,0 +1,18 @@
+// inisiasi dua variabel dengan nilai 10 dan 3
+// lakukan operasi penjumlahan, pengurangan, perkalian, pembagian, modulus
+// cetak semua hasil
+
+let nilai1 = 10;
+let nilai2 = 3;
+
+let penjumlahan = nilai1 + nilai2;
+let pengurangan = nilai1 - nilai2;
+let perkalian = nilai1 * nilai2;
+let pembagian = nilai1 / nilai2;
+let modulus = nilai1 % nilai2;
+
+console.log(penjumlahan);
+console.log(pengurangan);
+console.log(perkalian);
+console.log(pembagian);
+console.log(modulus);
