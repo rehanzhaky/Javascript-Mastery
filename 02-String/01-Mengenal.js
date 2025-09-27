2e93146dedbbe5151d3bac7f61e19887c40016d0
rehanzhaky <hannn9491@gmail.com>
2025-09-27 08:21:58 +0700

    mod: learn string

---
+++ 02-String/01-Mengenal.js
@@ -0,0 +1,15 @@
+let nama = 'raihan'
+console.log(nama)
+let asal = "tanjungpinang"
+console.log(asal)
+
+let nomor = 1
+console.log(nomor)
+let angka = '1'
+console.log(angka)
+
+console.log(typeof nomor)
+console.log(typeof angka)
+
+let perkenalan = "nama saya raihan zhaky kalia bisa panggil saya 'ree'"
+console.log(perkenalan)
